3b9951276a72ffac3fe9bc583dd60a678abc90d5
networkduchy <51041746+networkduchy@users.noreply.github.com>
2020-02-25 23:35:15 +1000

    updated the end loop flow

--- Cisco/Netconf/simplenetconfconnection.py
+++ Cisco/Netconf/simplenetconfconnection.py
@@ -5,4 +5,6 @@ router = {"host": "ios-xe-mgmt-latest.cisco.com", "port": "10000",
           "username": "developer", "password": "C1sco12345"}
 
 with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
-    m.close_session()
+    for capability in m.server_capabilities:
+        print('*' * 50)
+        print(capability)
